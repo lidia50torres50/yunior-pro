490aef9525cc1c9c0e770b4a637c40df96be4859
lidia50torres50 <103346724+lidia50torres50@users.noreply.github.com>
2022-05-15 10:50:13 -1200

    Update conf.py

--- conf.py
+++ conf.py
@@ -2,4 +2,4 @@ api_hash = "3be954be110a796e8c0b18f3604a5f2c"
 api_id = 16356512
 bot_token = "5258616181:AAFOXvaElqivzVWwGvPsJcxSfAl-nPEmMxs"
 authorized_users = [yunior132001]
-admin_user = '-1001590417745'
+admin_user = 'yunior132001'
